aa698c12d85de1481ef4614877cb1384c990a048
Julian Rabe <mail@julianrabe.com>
2020-05-30 21:09:30 +0200

    Moved browserify into callback

--- hooks/bundle-dependencies.js
+++ hooks/bundle-dependencies.js
@@ -6,16 +6,18 @@ module.exports = {
     'generate:after': async generator => {
 
         exec(`cd ${generator.targetDir} && npm i`, (error, stdout, stderr) => {
+
             if (error) {
                 console.error(`exec error: ${error}`);
                 return;
             }
+
             console.log(stdout);
             console.error(stderr);
-        });
 
-        new Browserify(`${generator.targetDir}/lib/browser.js`, {debug: true})
-            .bundle()
-            .pipe(Fs.createWriteStream(`${generator.targetDir}/lib/browser.bundle.js`));
+            new Browserify(`${generator.targetDir}/lib/browser.js`, {debug: true})
+                .bundle()
+                .pipe(Fs.createWriteStream(`${generator.targetDir}/lib/browser.bundle.js`));
+        });
     }
 };
